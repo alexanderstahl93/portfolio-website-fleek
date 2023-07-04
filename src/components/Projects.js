a405ed63740f0c434e58b933d2c6e8be59225e2a
Alexander Stahl <info@alexanderstahl.at>
2023-07-04 11:31:19 +0200

    New Project - Crypto Whale Tracker

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,6 @@
 // Import Assets
 import uniswap from '../assets/uniswap.png';
+import whale from '../assets/whale.jpg';
 import compound from '../assets/compound.png';
 import aave from '../assets/aave.png';
 
@@ -11,13 +12,13 @@ const Projects = () => {
             <div className="projects__cards">
 
                 <div className="projects__card">
-                    <h3>Uniswap</h3>
-                    <img src={uniswap} alt="Uniswap Swap Page" />
-                    <p>Web3 development by building a DEX with Uniswap.
+                    <h3>Crypto Whale Tracker</h3>
+                    <img src={whale} alt="Uniswap Swap Page" />
+                    <p>The Crypto Whale Tracker is designed to track large transfers of USDC (USD Coin) tokens on the Ethereum blockchain. 
+                        The script utilizes the Ethers.js library, which provides a convenient way to interact with the Ethereum network.
                     </p>
 
-                    <a href="#" target="_blank" className="button">...</a>
-                    <a href="#" target="_blank" className="button">...</a>
+                    <a href="https://github.com/alexanderstahl93/crypto-whale-tracker/tree/master" target="_blank" className="button">GitHub</a>
                 </div>
 
             </div>
